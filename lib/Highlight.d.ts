7dcad6ce2f44b34c685a90243c3b205e6787f85d
Christian <christian@trustle.io>
2021-03-15 19:41:58 -0300

    add dependency

--- lib/Highlight.d.ts
+++ lib/Highlight.d.ts
@@ -1,8 +1,10 @@
 import React, { CSSProperties } from 'react';
+import "Highlight.scss";
 export declare type HighlightProps = {
     children: React.ReactNode;
     className?: string;
     language: string;
     style?: CSSProperties | undefined;
+    showCopy?: boolean;
 };
 export default function Highlight(props: HighlightProps): JSX.Element;
